c0333f51495a9745dec6fe3bc7bedaf0ee3ac77c
fipnooone <fipnone@gmail.com>
2024-12-18 17:15:47 +0700

    fix: removed useless prop

--- src/Polybox.tsx
+++ src/Polybox.tsx
@@ -5,7 +5,7 @@ import { BoxProps } from './Polybox.types';
 const Polybox = <T extends React.ElementType = 'div'>({ as, ...props }: BoxProps<T>) => {
     const Element = (as || 'div') as React.ElementType;
 
-    return <Element {...props} as={props.as} />;
+    return <Element {...props} />;
 };
 
 export default Polybox;
